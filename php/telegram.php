97bf0d77912586be6b1af148b323f420964ab55e
InnaPalchynska <InnaPalchynska@gmail.com>
2020-02-09 00:33:24 +0200

    Update telegram.php

--- php/telegram.php
+++ php/telegram.php
@@ -4,7 +4,7 @@ $name = $_POST['user_name'];
 $phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
 $token = "936701731:AAH7NIc_6HJcgAaH2GGafWeECjdzJpzfF8k";
-$chat_id = "936701731";
+$chat_id = "-276912083";
 $arr = array(
   'Имя пользователя: ' => $name,
   'Телефон: ' => $phone,
@@ -15,7 +15,7 @@ foreach($arr as $key => $value) {
   $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$936701731:AAH7NIc_6HJcgAaH2GGafWeECjdzJpzfF8k}/sendMessage?chat_id={$936701731}&parse_mode=html&text={$txt}","r");
+$sendToTelegram = fopen("https://api.telegram.org/bot{$936701731:AAH7NIc_6HJcgAaH2GGafWeECjdzJpzfF8k}/sendMessage?chat_id={$-276912083}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
   header('Location: thank-you.html');
